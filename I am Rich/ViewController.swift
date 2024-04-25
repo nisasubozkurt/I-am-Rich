249ca7f2e20fd7a9fa81394a62e664fd524055bf
Nisasu <bozkurtnisasu@hotmail.com>
2024-04-25 20:44:48 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Su BOZKURT on 25.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
